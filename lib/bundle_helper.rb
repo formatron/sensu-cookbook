557b4e319b2f6cdc500bd83671460c5fc3e7dc01
Peter Halliday <pghalliday@gmail.com>
2015-11-27 14:00:56 +0100

    fix scope

--- lib/bundle_helper.rb
+++ lib/bundle_helper.rb
@@ -1,4 +1,4 @@
-module BundleHelper
+module Chef::Provider::BundleHelper
   GEM_BINARY = '/opt/sensu/embedded/bin/gem'
   RUBY_BINARY = '/opt/sensu/embedded/bin/ruby'
   BUNDLES_ROOT = '/usr/local/share/sensu/bundles'
